190f8ff213a72c265237b1cbac8136df758aa101
Courtney Anderson <hello+2703@courtneysarah.com>
2023-11-10 12:58:46 +1300

    Update index with environment variables

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,12 @@
 import server from './server.ts'
 
+import dotenv from 'dotenv'
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = dotenv.config()
+  if (envConfig.error) throw envConfig.error
+}
+
 const port = process.env.PORT || 3000
 
 server.listen(port, function () {

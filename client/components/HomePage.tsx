87920451656b9206a4f732ff9a85ea08c20648f0
Courtney Anderson <hello+2703@courtneysarah.com>
2023-11-09 21:25:19 +1300

    Add date component and initial css

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,19 +1,27 @@
-import { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient.ts'
-import { Link } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
+// import { getGreeting } from '../apiClient.ts'
+// import { Link } from 'react-router-dom'
 
 import Weather from './Weather.tsx'
-import Image from './Image.tsx'
+// import Image from './Image.tsx'
 import Affirmation from './Affirmation.tsx'
+import DateToday from './DateToday.tsx'
 
 
 function HomePage() {
   return (
-    <>
-      <Weather />
-      <Affirmation />
-      <Image />
-    </>
+    <div className='box'>
+      <div className='content'>
+        <Weather />
+        <Affirmation />
+      </div>
+      <div className='image'>
+        {/* <Image /> */}
+      </div>
+      <div className='date'>
+        <DateToday />
+      </div>
+    </div>
   )
 }
 
--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <>
+    <div className="box">
       <main>
         <Outlet />
       </main>
@@ -10,6 +10,6 @@ export default function Layout() {
       <footer>
         <p>Copyright &copy; A Powerpuff (girls) Initiative</p>
         <img src='https://media.tenor.com/c0_cRbtSZYYAAAAC/the-powerpuff-girls-cartoon-network.gif' alt='powerpuff girls flying' width='40'/></footer>
-    </>
+    </div>
   )
 }
